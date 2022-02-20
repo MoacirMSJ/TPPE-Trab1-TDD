f80884cc59e6f338212f11526fd32e4a41a6182e
joaopsc <joao.pedro@qualyteam.com.br>
2022-02-20 18:41:52 -0300

    criado teste de duplicação para cadastrar deducao
    Signed-off-by: joaopsc <joao.pedro@qualyteam.com.br>
    Co-authored-by: Moacir Mascarenha <moacirmascarenha86@gmail.com>

--- CalculadoraTributaria/src/test/DeducaoTest.java
+++ CalculadoraTributaria/src/test/DeducaoTest.java
@@ -11,9 +11,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DeducaoTest {
     @Test
     public void testCadastrarDeducao () {
-
         Deducao deducao = new Deducao("Salario", 10000f);
         assertEquals( deducao.getValor(), 10000f);
         assertEquals( deducao.getDescricao(), "Salario");
+    }
+
+    @Test
+    public void testDuplicacaoCadastratarDeducao () {
+        Deducao deducao = new Deducao("Funpresp", 480.20f);
+        assertEquals( deducao.getValor(), 480.20f);
+        assertEquals( deducao.getDescricao(), "Funpresp");
     }
 }
