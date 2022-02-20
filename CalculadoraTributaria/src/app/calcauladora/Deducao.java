e5ff23adebf0d5d6b52e9eaaea52f5e28fd0070f
moacirmsj <moacirmascarenha86@gmail.com>
2022-02-20 18:34:12 -0300

    Deducao falsificacao cadastro e retorno dos valores
    Signed-off-by: moacirmsj <moacirmascarenha86@gmail.com>
    Co-authored-by: João Pedro <jpcirqueira81@gmail.com>

--- CalculadoraTributaria/src/app/calcauladora/Deducao.java
+++ CalculadoraTributaria/src/app/calcauladora/Deducao.java
@@ -1,4 +1,19 @@
 package app.calcauladora;
 
 public class Deducao {
+    private String descricao;
+    private Float valor;
+
+    public Deducao(String descricao, Float valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public Float getValor() {
+        return 10000f;
+    }
+
+    public String getDescricao() {
+        return "Salario";
+    }
 }
--- CalculadoraTributaria/src/test/DeducaoTest.java
+++ CalculadoraTributaria/src/test/DeducaoTest.java
@@ -11,8 +11,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DeducaoTest {
     @Test
     public void testCadastrarDeducao () {
-        Deducao deducao = new Deducao("Salario", 10000);
-        assertEquals( deducao.getValor(), 10000);
+
+        Deducao deducao = new Deducao("Salario", 10000f);
+        assertEquals( deducao.getValor(), 10000f);
         assertEquals( deducao.getDescricao(), "Salario");
     }
 }
