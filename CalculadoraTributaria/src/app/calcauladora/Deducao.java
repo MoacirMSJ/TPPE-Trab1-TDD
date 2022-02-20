b1b94e142f951d99d15fa128cf3bd703219f54d0
moacirmsj <moacirmascarenha86@gmail.com>
2022-02-20 20:25:09 -0300

    Deducao criado constructor e utilizada falsificacao
    Signed-off-by: moacirmsj <moacirmascarenha86@gmail.com>
    Co-authored-by: João Pedro <jpcirqueira81@gmail.com>

--- CalculadoraTributaria/src/app/calcauladora/Deducao.java
+++ CalculadoraTributaria/src/app/calcauladora/Deducao.java
@@ -1,12 +1,24 @@
 package app.calcauladora;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 public class Deducao {
     private String descricao;
     private Float valor;
+    private String nomeDependente = "";
+    private String dataNascimentoDependente = "";
 
     public Deducao(String descricao, Float valor) {
         this.descricao = descricao;
         this.valor = valor;
+    }
+
+    public Deducao(String nomeDependente, String dataNascimento) {
+        this.descricao = "Dependente";
+        this.nomeDependente = nomeDependente;
+        this.dataNascimentoDependente = dataNascimento;
+        this.valor = 189.59f;
     }
 
     public Float getValor() {
@@ -15,5 +27,12 @@ public class Deducao {
 
     public String getDescricao() {
         return this.descricao;
+    }
+
+    public Dictionary getDependenteInfo() {
+        Dictionary dependenteInfo = new Hashtable();
+        dependenteInfo.put("nome", "João Pedro");
+        dependenteInfo.put("dataNascimento", "01/01/2005");
+        return dependenteInfo;
     }
 }
