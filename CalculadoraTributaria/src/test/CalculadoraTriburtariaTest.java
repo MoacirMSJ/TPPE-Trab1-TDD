1b77b0dc7371f7695b8566c486375cc6fe5ec229
joaopsc <joao.pedro@qualyteam.com.br>
2022-02-24 21:56:36 -0300

    Adicionado teste para Cadastro de rendimento.
    Signed-off-by: joaopsc <joao.pedro@qualyteam.com.br>

---
+++ CalculadoraTributaria/src/test/CalculadoraTriburtariaTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import app.calcauladora.CalculadoraTributaria;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculadoraTriburtariaTest {
+
+
+    @Test
+    public void testCadastroUmRendimento () {
+        CalculadoraTributaria calc = new CalculadoraTributaria();
+        calc.CadastrarRendimentos("salario", 10000f);
+        assertEquals( calc.getTotalRendimentos(), 10000f);
+
+    }
+
+    @Test
+    public void testCadastroDoisRendimento () {
+        CalculadoraTributaria calc = new CalculadoraTributaria();
+        calc.CadastrarRendimentos("salario", 10000f);
+        calc.CadastrarRendimentos("aluguel", 2000f);
+        assertEquals( calc.getTotalRendimentos(), 12000f);
+    }
+}
