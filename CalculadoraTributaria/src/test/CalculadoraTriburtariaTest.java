d63a6547179a107b03435c33825afee96313bff8
joaopsc <joao.pedro@qualyteam.com.br>
2022-02-24 23:34:34 -0300

    ajustado teste de cadastro de rendimento para teste  parametrizado
    Signed-off-by: joaopsc <joao.pedro@qualyteam.com.br>
    Co-authored-by: lucasA27 <lucas.alexandre.df@gmail.com>
    Co-authored-by: MatheusEstanislau <matheus.estanislau7@gmail.com>
    Co-authored-by: Moacir Mascarenha<moacirmascarenha86@gmail.com>

--- CalculadoraTributaria/src/test/CalculadoraTriburtariaTest.java
+++ CalculadoraTributaria/src/test/CalculadoraTriburtariaTest.java
@@ -1,25 +1,61 @@
 package test;
 
 import app.calcauladora.CalculadoraTributaria;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+import java.util.Collection;
 
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
 public class CalculadoraTriburtariaTest {
 
+    CalculadoraTributaria simul;
 
-    @Test
-    public void testCadastroUmRendimento () {
-        CalculadoraTributaria calc = new CalculadoraTributaria();
-        calc.CadastrarRendimentos("salario", 10000f);
-        assertEquals( calc.getTotalRendimento(), 10000f);
+    Object[][] rendimentos;
+    float valorEsperado;
+
+    public CalculadoraTriburtariaTest(Object[][] rendimentos, float valorEsperado) {
+        this.rendimentos = rendimentos;
+        this.valorEsperado = valorEsperado;
+    }
+
+    @Parameters
+    public static Collection<Object[]> getParameters() {
+
+        Object[][] respostas = new Object[][]{
+                {new Object[][]{
+                        {"salario", 15000f},
+                }, 15000f},
+                {new Object[][]{
+                        {"salario", 8000f},
+                        {"dividendo", 3500f}
+                }, 11500f},
+                {new Object[][]{
+                        {"salario", 5000f},
+                        {"dividendo", 300f},
+                        {"nota fiscal ", 4000f}
+                }, 9300f}
+        };
+
+        return Arrays.asList(respostas);
+    }
+
+    @Before
+    public void setup() {
+        simul = new CalculadoraTributaria();
     }
 
     @Test
-    public void testCadastroDoisRendimento () {
-        CalculadoraTributaria calc = new CalculadoraTributaria();
-        calc.CadastrarRendimentos("salario", 10000f);
-        calc.CadastrarRendimentos("aluguel", 2000f);
-        assertEquals( calc.getTotalRendimento(), 12000f);
+    public void testCadastroRendimentosParametrizado() {
+        for (Object[] o : rendimentos) {
+            simul.CadastrarRendimentos((String) o[0], (float) o[1]);
+        }
+        assertEquals(valorEsperado, simul.getTotalRendimento(), 0f);
     }
 }
