bf70917df52275c10c73d9655b5be37a98e481f5
moacirmsj <moacirmascarenha86@gmail.com>
2022-04-22 23:53:58 -0300

    Refactoring: extrair metodo
    Mau(s) cheiro(s) identificado(s): metodo longo
    
    Descrição da refatoração: aplicada extracao de metodo na deducao, criando um metodo especifico para a
    validacao deixando mais correto com o nome do metodo explicando sua funcao.
    
    Classes/métodos/atributos afetados: Deducao,
    * Método(Construtor) Deducao foi dividido nos métodos em dois Deducao e validarDeducao
    * Método validarDeducao foi extraido a partir do metodo(Construtor) Deducao para diminuir o tamanho do metodo e suas responsabilidades.
    
    Signed-off-by: moacirmsj <moacirmascarenha86@gmail.com>
    Co-authored-by: João Pedro <jpcirqueira81@gmail.com>

--- CalculadoraTributaria/src/app/calculadora/Deducao.java
+++ CalculadoraTributaria/src/app/calculadora/Deducao.java
@@ -13,16 +13,18 @@ public class Deducao {
     private String dataNascimentoDependente = "";
 
     public Deducao(String descricao, Float valor) {
+        this.validarDeducao(descricao,valor);
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    private void validarDeducao(String descricao, Float valor) {
         if (valor <= 0){
             throw new ValorDeducaoInvalidoException("Valor deve ser maior do que zero");
         }
-
         if (descricao.isEmpty()){
             throw new DescricaoEmBrancoException("Descrição não informada");
         }
-
-        this.descricao = descricao;
-        this.valor = valor;
     }
 
     public Deducao(String nomeDependente, String dataNascimento) {
