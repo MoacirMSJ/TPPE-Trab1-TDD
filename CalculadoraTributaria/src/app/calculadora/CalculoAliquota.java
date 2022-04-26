a46356d002066de1c67f871c523754fab04935ad
gleal17 <guilhermelml@gmail.com>
2022-04-25 23:22:30 -0300

    Refactoring: extrair constante Mau-cheiro identificado:
    Utilização de variável temporária para cálculo de expressão, modificação de variável temporaria
    
    Classes/métodos/atributos afetados:
    CalculoAliquota.class, AliquotaP.class
    computar()
    Atributo base foi dividido em diversos atributos, um para cada faixa

--- CalculadoraTributaria/src/app/calculadora/CalculoAliquota.java
+++ CalculadoraTributaria/src/app/calculadora/CalculoAliquota.java
@@ -1,6 +1,16 @@
 package app.calculadora;
 
 class CalculoAliquota {
+    //EXTRACAO DE CONSTANTES  DO METODO OBJETO REFERENTE A AliquotaP
+    public static final float FAIXA1 = 1903.98f;
+    public static final float FAIXA2 = 922.67f;
+    public static final float FAIXA3 = 924.40f;
+    public static final float FAIXA4 = 913.63f;
+    public static final float ALIQ1 = 0.075f;
+    public static final float ALIQ2 = 0.15f;
+    public static final float ALIQ3 = 0.225f;
+    public static final float ALIQ4 = 0.275f;
+
     private AliquotaP _aliquotaP;
     private float soma = 0f;
     private float base = 0f;
@@ -11,18 +21,18 @@ class CalculoAliquota {
 
 
     public float computar() {
-        if (this.base > 1903.98f) {
-            this.base -= 1903.98f;
-            this.soma += ((Math.min(this.base, 922.67f) * 0.075f));
-            if (this.base > 922.67f) {
-                this.base -= 922.67f;
-                this.soma += ((Math.min(this.base, 924.40f) * 0.15f));
-                if (this.base > 924.40f) {
-                    this.base -= 924.40f;
-                    this.soma += ((Math.min(this.base, 913.63f) * 0.225f));
-                    if (this.base > 913.63f) {
-                        this.base -= 913.63f;
-                        this.soma += ((this.base * 0.275f));
+        if (this.base > FAIXA1) {
+            this.base -= FAIXA1;
+            this.soma += ((Math.min(this.base, FAIXA2) * ALIQ1));
+            if (this.base > FAIXA2) {
+                this.base -= FAIXA2;
+                this.soma += ((Math.min(this.base, FAIXA3) * ALIQ2));
+                if (this.base > FAIXA3) {
+                    this.base -= FAIXA3;
+                    this.soma += ((Math.min(this.base, FAIXA4) * ALIQ3));
+                    if (this.base > FAIXA4) {
+                        this.base -= FAIXA4;
+                        this.soma += ((this.base * ALIQ4));
                     }
                 }
             }
